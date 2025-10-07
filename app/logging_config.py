d56cb155bfeb3160348fc3ec2cbb1b797ab2f73a
edu473 <eduardomora473@gmail.com>
2025-10-07 00:39:24 +0000

    cambio regresado

--- app/logging_config.py
+++ app/logging_config.py
@@ -4,7 +4,7 @@ from logging.config import dictConfig
 
 LOGGING_CONFIG = {
     "version": 1,
-    "disable_existing_loggers": True,
+    "disable_existing_loggers": False,
     "formatters": {
         "default": {
             "format": "%(asctime)s - %(levelname)s - %(name)s - %(message)s",
