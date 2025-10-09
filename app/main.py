3e4638e62760d2e92e0c07fb6a99ebd43869a4f3
edu473 <eduardomora473@gmail.com>
2025-10-09 16:55:23 +0000

    warmup fixed

--- app/main.py
+++ app/main.py
@@ -1,9 +1,13 @@
 # app/main.py
-
+import asyncio
+import logging
+from contextlib import asynccontextmanager
+from . import logic
 from fastapi import FastAPI
 from .logging_config import setup_logging # IMPORTAMOS LA FUNCIÓN
 
 setup_logging()
+logger = logging.getLogger("app")
 
 from contextlib import asynccontextmanager
 from .routers import subscribers
@@ -11,17 +15,27 @@ from .routers import subscribers
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    # Código que se ejecuta ANTES de que la aplicación empiece a aceptar peticiones
-    # Creamos una tarea en segundo plano para no bloquear el inicio.
-    asyncio.create_task(logic.warmup_connections())
-    
-    yield  # La aplicación se ejecuta aquí
+    """
+    Gestiona el ciclo de vida de la aplicación.
+    Ejecuta tareas de inicio y limpieza.
+    """
+    async def warmup_task_wrapper():
+        try:
+            logger.info("Iniciando tarea de calentamiento de conexiones en segundo plano...")
+            await logic.warmup_connections()
+            logger.info("Tarea de calentamiento de conexiones finalizada exitosamente.")
+        except Exception as e:
+     
+            logger.error(f"La tarea de calentamiento de conexiones falló: {e}", exc_info=True)
+    asyncio.create_task(warmup_task_wrapper())
 
-# Crea la instancia principal de la aplicación FastAPI.
-# Los títulos y versiones aparecerán en la documentación de Swagger.
+    yield 
+
+
 app = FastAPI(
     title="FastAPI Simple Subscribers EDUSOFT",
-    version="0.1.0"
+    version="0.1.0",
+    lifespan=lifespan
 )
 
 # Incluye todas las rutas definidas en el archivo subscribers.py
